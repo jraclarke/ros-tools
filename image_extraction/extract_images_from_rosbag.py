967bf35e270ae9ee2cf60e42ac25084e7ff1eca7
James Clarke <jraclarke@hotmail.com>
2023-11-02 11:13:47 +0000

    Merge uncompressed and compressed image output into one script.

--- image_extraction/extract_images_from_rosbag.py
+++ image_extraction/extract_images_from_rosbag.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python
+"""
+Read image topics from a bagfile and output them to a png (can be changed) with a timestamp 
+and topic as the name.
+Works for either compressed or uncompressed image topics.
+
+Todo:
+Error handling.
+"""
 import tqdm
 import rosbag  # only python2.7 (http://wiki.ros.org/kinetic/Installation/Ubuntu)
 import numpy as np
-
 import cv2
-import matplotlib.pyplot as plt
 import os
 import argparse
 import rospy
@@ -11,32 +18,30 @@ import datetime
 from cv_bridge import CvBridge
 
 parser = argparse.ArgumentParser()
-parser.add_argument(dest='target_dir', type=str, help="The path to the files' directory.")
+parser.add_argument(dest='target_dir', type=str, help="The path to the bag files' directory.")
 parser.add_argument(dest='in_file', type=str, help="The name of the input file.")
 parser.add_argument(dest='offset', type=int, help="How many hours to offset")
+parser.add_argument(dest='topics', type=str, help="csv of image topics to output images for.")
+parser.add_argument("--compressed", action="store_true", help="Input image topics are compressed")
+parser.set_defaults(compress=False)
 args = parser.parse_args()
 
 bridge = CvBridge()
 
-## Step1 - Read .rosbag
+# Read rosbag
 bag = rosbag.Bag(os.path.join(args.target_dir, args.in_file))
 
-## Step2 - Read Compressed Images Packets
 print(' - start_time        : ', bag.get_start_time())
 print(' - end_time          : ', bag.get_end_time())
 
-topics = ['/port_0/camera_0/image_raw', '/port_0/camera_1/image_raw']
+topics = args.topics.split(',')
 
 for idx, (topic, msg, t) in enumerate(bag.read_messages(topics=topics)):
-    print(t)
 
-    if topic == '/port_0/camera_0/image_raw':
-        prefix = 'STREETDRONE.FRONTCAM_IMG.'
-    elif topic == '/port_0/camera_1/image_raw':
-        prefix = 'STREETDRONE.REARCAM_IMG.'
-
-    #img = cv2.imdecode(np.fromstring(msg.data, np.uint8), cv2.IMREAD_COLOR)
-    img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    if args.compressed:
+        img = cv2.imdecode(np.fromstring(msg.data, np.uint8), cv2.IMREAD_COLOR)
+    else:
+        img = bridge.imgmsg_to_cv2(msg, "bgr8")
 
     dt1 = datetime.datetime.utcfromtimestamp(t.to_sec())
     dt1 = dt1 + datetime.timedelta(hours=args.offset)
@@ -47,7 +52,8 @@ for idx, (topic, msg, t) in enumerate(bag.read_messages(topics=topics)):
         t_ms = 0
     timestamp = t_str + '.' + str(t_ms).zfill(3)
 
-    filename = os.path.join(args.target_dir, prefix + timestamp + ".png")
+    filename = os.path.join(args.target_dir, timestamp + topic.replace('/', '.') + ".png")
     # Note that imwrite detects extension for writing
     cv2.imwrite(filename, img)
     print('Written file {0}'.format(filename))
+
